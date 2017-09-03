1a66619a76a2f94e8180634249fefc5c11d92689
Sergey Chernyshev <github.com@antispam.sergeychernyshev.com>
2017-09-03 01:05:10 -0400

    Make footer sticky, closes issue #1

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,6 @@ import { element } from "prop-types";
 
 import Navbar from "react-bootstrap/lib/Navbar";
 
-const startYear = 2017;
-const year = new Date().getFullYear();
-const yearMessage = year > startYear ? ({startYear} - {year}) : year;
-
 const App = props => (
   <div>
     <Navbar inverse collapseOnSelect>
@@ -25,16 +21,6 @@ const App = props => (
     <div className="container">
       {props.children}
     </div>
-
-    <footer className="panel-footer" style={{marginTop: '5em', paddingBottom: '5em'}}>
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-12">
-            © {yearMessage} Sergey Chernyshev
-          </div>
-        </div>
-      </div>
-    </footer>
   </div>
 );
 
